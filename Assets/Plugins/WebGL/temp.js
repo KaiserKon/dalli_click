be5bf9cc70b92752e62a6192762a119f05a8db79
KaiserK <konstantin.kaiser@de.markant.com>
2022-08-24 23:38:57 +0200

    remove automatic delete of input

--- Assets/Plugins/WebGL/temp.js
+++ Assets/Plugins/WebGL/temp.js
@@ -1,25 +1,27 @@
 var ImageUploaderPlugin = {
   ImageUploaderCaptureClick: function() {
     if (!document.getElementById('ImageUploaderInput')) {
-      const fileInput = document.createElement('input');
-      fileInput.type = 'file';
-      fileInput.id = 'ImageUploaderInput';
+      var fileInput = document.createElement('input');
+      fileInput.setAttribute('type', 'file');
+      fileInput.setAttribute('id', 'ImageUploaderInput');
       fileInput.setAttribute('multiple', '');
       fileInput.setAttribute('accept', 'png, jpg');
       // fileInput.style.visibility = 'hidden';
       fileInput.onchange = function(event) {
         for (let index = 0; index < event.target.files.length; index++) {
           const file = event.target.files[index];
-          SendMessage('Canvas', 'FileSelected', URL.createObjectURL(file) + '|' + file.fileName)
+          SendMessage('Canvas', 'FileSelected', URL.createObjectURL(file))
         }
       }
       document.body.appendChild(fileInput);
     }
+    /*
     const OpenFileDialog = function() {
       document.getElementById('ImageUploaderInput').click();
       document.getElementById('unity-canvas').removeEventListener('click', OpenFileDialog);
     };
     document.getElementById('unity-canvas').addEventListener('click', OpenFileDialog, false);
+    */
   }
 };
 mergeInto(LibraryManager.library, ImageUploaderPlugin);
--- docs/Build/docs.framework.js
+++ docs/Build/docs.framework.js
@@ -1284,24 +1284,19 @@ function stackTrace() {
 
 function _ImageUploaderCaptureClick() {
  if (!document.getElementById("ImageUploaderInput")) {
-  const fileInput = document.createElement("input");
-  fileInput.type = "file";
-  fileInput.id = "ImageUploaderInput";
+  var fileInput = document.createElement("input");
+  fileInput.setAttribute("type", "file");
+  fileInput.setAttribute("id", "ImageUploaderInput");
   fileInput.setAttribute("multiple", "");
   fileInput.setAttribute("accept", "png, jpg");
   fileInput.onchange = function(event) {
    for (let index = 0; index < event.target.files.length; index++) {
     const file = event.target.files[index];
-    SendMessage("Canvas", "FileSelected", URL.createObjectURL(file) + "|" + file.fileName);
+    SendMessage("Canvas", "FileSelected", URL.createObjectURL(file));
    }
   };
   document.body.appendChild(fileInput);
  }
- const OpenFileDialog = function() {
-  document.getElementById("ImageUploaderInput").click();
-  document.getElementById("unity-canvas").removeEventListener("click", OpenFileDialog);
- };
- document.getElementById("unity-canvas").addEventListener("click", OpenFileDialog, false);
 }
 
 var JS_Accelerometer = null;
